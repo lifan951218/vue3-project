b5fea1f8541e443bbb9b1bba721e43bce2386085
lifan <874508852@qq.com>
2023-03-30 16:37:46 +0800

    新版ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-config-provider :locale="zhCn">
+    <el-config-provider :locale="zhCn" namespace="el">
         <router-view />
     </el-config-provider>
 </template>
@@ -7,6 +7,14 @@
 <script setup lang="ts">
 import { ElConfigProvider } from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.body.style.setProperty('--el-color-primary', '#63b26e');
+  document.body.style.setProperty('--el-color-primary-light-9', '#F5FBF0');
+  document.body.style.setProperty('--el-color-primary-light-3', '#95d475');
+})
+
 </script>
 <style>
 @import './assets/css/main.css';
--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,45 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
 	base: './',
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// 自定义的主题色
+				additionalData: `@use "./src/styles/element/index.scss" as *;`,
+			},
+		},
+	},
 	plugins: [
 		vue(),
 		VueSetupExtend(),
-		AutoImport({
-			resolvers: [ElementPlusResolver()]
-		}),
+		// AutoImport({
+		// 	resolvers: [ElementPlusResolver()]
+		// }),
+		// Components({
+		// 	resolvers: [ElementPlusResolver()]
+		// }),
+		//按需引入element
 		Components({
-			resolvers: [ElementPlusResolver()]
-		})
+			resolvers: [
+				ElementPlusResolver({
+					// 按需引入修改主题色添加这一行，使用预处理样式
+					importStyle: "sass",
+				}),
+			],
+		}),
+		// 自动引入，注意这里需要在Components之后引入
+		AutoImport({
+			resolvers: [
+				ElementPlusResolver({
+					// 自动引入修改主题色添加这一行，使用预处理样式，不添加将会导致使用ElMessage，ElNotification等组件时默认的主题色会覆盖自定义的主题色
+					importStyle: "sass",
+				}),
+			],
+		}),
 	],
 	optimizeDeps: {
 		include: ['schart.js']
 	}
 });
+
+
